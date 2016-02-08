6103eb61be5b6f4e8c8fcdf71c83c920feb6880f
Estudiante <Estudiante@168.176.25.111>
2016-02-08 14:43:30 -0500

    prueba

--- src/holamundo/Holamundo.java
+++ src/holamundo/Holamundo.java
@@ -15,7 +15,8 @@ public class Holamundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-            system.out.println("hola mundo");
+            System.out.println("hola mundo");
+            System.out.println("Pipe es un petardo(a ratos)");
 
 // TODO code application logic here
     }
